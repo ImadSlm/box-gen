88b3c9847d56cc31d394c9e63aa4fd0eadbadee0
Imad Saleem <105483840+ImadSlm@users.noreply.github.com>
2024-04-22 16:50:10 +0200

    Add h2 title on header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,6 +4,7 @@ export default function Header() {
             <h1 className="text-xl text-center uppercase font-semibold">
                 Boxy generator
             </h1>
+            <h2 className="py-3 text-center font-semibold">- Create a box-shadow and copy the code for your script -</h2>
         </div>
     );
 }
